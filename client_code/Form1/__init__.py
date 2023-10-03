0b067c501faa72df33c05fb1b6aa9de41783c679
showvik.sizan@gmail.com <showvik.sizan@gmail.com>
2023-10-03 06:29:03 +0000

    Created LICENSE.txt; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,6 +8,10 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+  def handle_single_target_variable(self):
+    """Handle the logic for when there's only one target variable."""
+    unique_values = anvil.server.call('get_unique_values_for_feature', self.target_variable_picker.selected_value)
+    self.class_picker.items = unique_values
 
   def csv_uploader_change(self, file, **event_args):
     """This method is called when a file is loaded into the csv_uploader."""
@@ -15,14 +19,13 @@ class Form1(Form1Template):
     categorical_features = anvil.server.call('process_data', file)
     self.target_variable_picker.items = categorical_features
 
-  def target_variable_picker_show(self, **event_args):
-      """This method is called when a target variable is selected."""
-      # Call the server function to get unique values for the selected target variable and populate the class_picker dropdown
-      unique_values = anvil.server.call('get_unique_values_for_feature', self.target_variable_picker.selected_value)
-      self.class_picker.items = unique_values
+      # If there's only one categorical feature, set the target_variable_picker to that feature
+    # and manually invoke the logic you want to execute
+    if len(categorical_features) == 1:
+        self.target_variable_picker.selected_value = categorical_features[0]
+        self.handle_single_target_variable()
 
 
-  
 
 
   def submit_button_for_processing_click(self, **event_args):
@@ -32,6 +35,17 @@ class Form1(Form1Template):
       
       # Display the SHAP summary plot in the shap_displayer
       self.shap_summary_displayer.source = plot_media
+
+  def target_variable_picker_change(self, **event_args):
+      """This method is called when a target variable is selected."""
+      # Call the server function to get unique values for the selected target variable and populate the class_picker dropdown
+      unique_values = anvil.server.call('get_unique_values_for_feature', self.target_variable_picker.selected_value)
+      self.class_picker.items = unique_values
+
+
+
+
+
 
 
 
