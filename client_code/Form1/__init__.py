f76692a3fb697fed7b08f3ebe31dbfa4437e2605
showvik.sizan@gmail.com <showvik.sizan@gmail.com>
2023-10-03 04:44:07 +0000

    Edited ServerModule1, Form1 and custom package dependencies

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -9,9 +9,7 @@ class Form1(Form1Template):
 
     # Any code you write here will run before the form opens.
 
-
-
-def csv_uploader_change(self, file, **event_args):
+  def csv_uploader_change(self, file, **event_args):
     """This method is called when a file is loaded into the csv_uploader."""
     # Call the server function to process the uploaded file
     categorical_features = anvil.server.call('process_data', file)
@@ -19,15 +17,19 @@ def csv_uploader_change(self, file, **event_args):
     # Populate the target_variable_picker with the list of categorical features
     self.target_variable_picker.items = categorical_features
 
-def submit_button_for_processing_click(self, **event_args):
+  def submit_button_for_processing_click(self, **event_args):
     """This method is called when the submit_button_for_processing is clicked."""
     # Call the server function to process the data, train the model, and get SHAP values
     plot_media, shap_summary_data = anvil.server.call('train_and_get_shap', self.csv_uploader.file, self.target_variable_picker.selected_value)
     
     # Display the SHAP summary plot in the shap_displayer
-    self.shap_displayer.source = plot_media
+    self.shap_summary_displayer.source = plot_media
     
     # If you have a DataGrid or other component to display the SHAP summary DataFrame, you can set its items here
     # For example:
     # self.data_grid_1.items = shap_summary_data
+
+
+
+
 
